4b9f938cdfe1f732256aa9829c3fc3adf7b4498f
Kshir Ramruthan <kshirramruthan@gmail.com>
2016-04-16 17:30:26 +0200

    Setup and name added

---
+++ DisplayNames.cpp
@@ -0,0 +1,37 @@
+#include<allegro5/allegro.h>
+#include<allegro5/allegro_native_dialog.h>
+#include<allegro5/allegro_ttf.h>
+#include<allegro5/allegro_font.h>
+
+#define width 800
+#define height 600
+
+int main()
+{
+	if (!al_init())
+	{
+		al_show_native_message_box(NULL, NULL, NULL, "Could not initalize allegro5", NULL, NULL);
+	}
+
+	al_set_new_display_flags(ALLEGRO_FULLSCREEN_WINDOW);
+	ALLEGRO_DISPLAY *display = al_create_display(width, height);
+	al_set_window_position(display, 200, 100);
+	al_set_window_title(display, "Title goes here");
+
+	if (!display)
+	{
+		al_show_native_message_box(display, "Sample", "Display Settings", "Could not initalize allegro Window", NULL, NULL);
+	}
+
+	al_init_font_addon();
+	al_init_ttf_addon();
+
+	ALLEGRO_FONT *font = al_load_font("orbitron-black.ttf", 36, NULL);
+	al_draw_text(font, al_map_rgb(44, 177, 255), width / 2, 150, ALLEGRO_ALIGN_CENTRE, "Kshir Ramruthan");
+
+	al_flip_display();
+	al_destroy_font(font);
+	al_rest(5.0);
+	al_destroy_display(display);
+	return 0;
+}
